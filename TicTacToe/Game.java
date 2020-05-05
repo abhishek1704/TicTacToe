871019d8c89304a9b50b62a2cc74452c169f56a4
abhishek1704 <velankarabhishek@gmail.com>
2020-05-05 23:23:05 +0530

    added comments

--- TicTacToe/Game.java
+++ TicTacToe/Game.java
@@ -19,6 +19,7 @@ public class Game {
         displayCurrentBoard();
     }
 
+    
     public void startGame() {
 
         int row,column;
@@ -28,9 +29,11 @@ public class Game {
         System.out.println("Player 1 : X" + "\n" + "Player 2 : O");
         System.out.println("Start the Game ::");
 
+         
         for (int i=0;i<9;i++) {
+            // After five moves each time it will check for result
             if(i>4) {
-                flag = checkResult();
+                flag = checkResult(); // checking result, will return true if game is over without a tie
 
                 if(flag) {
                     System.out.print("\nResult : ");
@@ -46,7 +49,7 @@ public class Game {
             }
 
             System.out.println(" ");
-            displayCurrentBoard();
+            displayCurrentBoard(); // will display current situation of a board
 
             System.out.println("\n\nPlayer " + (i%2+1) + " : " );
 //            char temp = scanner.next().charAt(0);
@@ -54,6 +57,7 @@ public class Game {
             System.out.println("Enter the location on the board ( 1 - 9 ) :");
             int loc = scanner.nextInt();
 
+            // Calculating row and column number for input
             if(loc<=3) {
                 row = 1;
             } else if(loc>3 && loc<=6) {
@@ -67,6 +71,8 @@ public class Game {
             if(column==0) {
                 column = 3;
             }
+            
+            // Validation for already filled location
             if (board[row-1][column-1]=='X' || board[row-1][column-1]=='O') {
                 i--;
                 System.out.println("\nLocation is already filled !");
@@ -81,6 +87,7 @@ public class Game {
             }
 
         }
+        // Checking for tie
         if (!flag) {
             System.out.println("\n\nTHE GAME HAS TIED !!\n");
             displayCurrentBoard();
@@ -104,12 +111,15 @@ public class Game {
 
     private boolean checkResult() {
 
+        // row wise check
         for (int i=0;i<3;i++) {
            if (board[i][0]==board[i][1] && board[i][1] == board[i][2] && board[i][1]!=' ') {
                winner = board[i][0];
                return true;
            }
         }
+        
+        // column wise check
         for (int i=0;i<3;i++) {
             if (board[0][i]==board[1][i] && board[1][i] == board[2][i] && board[i][1]!=' ') {
                 winner = board[0][i];
@@ -117,11 +127,14 @@ public class Game {
             }
         }
         int i=0;
+        
+        // check for major diagonal
         if (board[i][i]==board[i+1][i+1] && board[i+1][i+1]==board[i+2][i+2] && board[i][1]!=' ') {
             winner = board[0][i];
             return true;
         }
 
+        //check for minor diagonal
         if (board[i][2]==board[i+1][1] && board[i+1][1]==board[i+2][0] && board[i][2]!=' ') {
             winner = board[i][2];
             return true;
